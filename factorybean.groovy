86f0c8aa4012c2ecbee68a48c15573f239999b60
SARATH1994 <sarathgogineni@outlook.com>
2021-02-18 14:36:17 +0530

    Update factorybean.groovy

--- factorybean.groovy
+++ factorybean.groovy
@@ -1,8 +1,18 @@
-VCSF vcsf = new VCSF()
-VCS vcs = vcsf.getInstance("svnvv")
-vcs.checkout()
+VCSF vcsf = new VCSF()  //Returns VCSF obj
+
+//VCS git = vcsf.getInstance("gitsdas")
+
+VCS svn = vcsf.getInstance("svnfs")  //geeting Obj based on name 
+
+//Gitone one = new Gitone()
+
+Svnone SVN = new Svnone()  // Instantiate class that you have 
+
+svn.checkout(SVN,"Sarath")  // passing obj to method 
 
 
+//git.checkout()
+ 
 class VCSF  {
     
      VCS getInstance(str) {
@@ -14,22 +24,60 @@ class VCSF  {
 }
 
 
+
+
 interface VCS {                                         
 
     void checkout()
+    
+    //void checkout(obj,name)
 }
 
 class git implements VCS {
-    @Override
+    
     void checkout() {
-        println "I am from git"
+        println("original git")
     }
+    
+    void checkout(obj,name) {
+        obj.checkout(name)
+    
+    }
+    
+    
 }
 
 class svn implements VCS {
-    @Override
+    
     void checkout() {
         println "I am from SVN"
     }
+    
+    void checkout(obj,name) {
+        obj.checkout(name)
+    
+    }
 }
+
+class Svnone {
+    
+    void checkout(name) {
+        println "I am checkout from Svnone "+name
+    }
+}
+
+class Gitone {
+    
+    void checkout() {
+        println "I am checkout from Gitone "
+    }
+}
+
+class Gittwo {
+    
+    void checkout() {
+        println "I am checkout from Gittwo "
+    }
+}
+
 
